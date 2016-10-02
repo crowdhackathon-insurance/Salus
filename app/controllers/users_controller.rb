0858225ce8a7b836e694966763d6729d3658c002
Spyros Avlonitis <spyrosavl@gmail.com>
2016-10-02 12:48:46 +0300

    dssdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user_api
-      token = request.headers["Content-Type"]
+      token = request.headers["user_token"]
       @user = User.find_by(user_token:token)
       if not @user then 
 
